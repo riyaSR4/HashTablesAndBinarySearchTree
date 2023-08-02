d5d3eab17c00e59d0d9f9363f19ee77f360d01eb
riyaSR4 <139098298+riyaSR4@users.noreply.github.com>
2023-08-02 13:12:20 +0530

    Update Program.cs

--- HashTablesAndBinarySearchTree/Program.cs
+++ HashTablesAndBinarySearchTree/Program.cs
@@ -6,14 +6,6 @@ namespace HashTablesAndBinarySearchTree
     {
         static void Main(string[] args)
         {
-            bool flag = true;
-            while (flag)
-            {
-                Console.WriteLine("Enter the option to execute\n 1.Hash Table\n 2.Binary Search Tree\n 3.Exit");
-                int option = Convert.ToInt32(Console.ReadLine());
-                switch (option)
-                {
-                    case 1:
                         Console.WriteLine("Enter the string");
                         string statement = Console.ReadLine();
                         string[] sentence = statement.Split(" ");
@@ -36,31 +28,6 @@ namespace HashTablesAndBinarySearchTree
                                 count++;
                             }
                         }
-                        break;
-                    case 2:
-                        BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
-                        binarySearch.Insert(30);
-                        binarySearch.Insert(70);
-                        binarySearch.Insert(22);
-                        binarySearch.Insert(40);
-                        binarySearch.Insert(60);
-                        binarySearch.Insert(95);
-                        binarySearch.Insert(11);
-                        binarySearch.Insert(65);
-                        binarySearch.Insert(3);
-                        binarySearch.Insert(16);
-                        binarySearch.Insert(63);
-                        binarySearch.Insert(67);
-                        binarySearch.Display();
-                        binarySearch.GetSize();
-                        bool result = binarySearch.IfExists(67, binarySearch);
-                        Console.WriteLine(result);
-                        break;
-                    case 3:
-                        flag = false;
-                    break;
-                }
-            }
         }
     }
 }
